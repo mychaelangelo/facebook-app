8b670f01d65053f910fb6be60e7e173b27b4ecf8
mychaelangelo <michael.tefula@gmail.com>
2014-09-29 22:34:07 +0100

    Add redis to heroku

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,10 @@
 # set global variable for Redis use across the application
-REDIS = Redis::Namespace.new("facebook_app", :redis => Redis.new)
+
+#REDIS = Redis::Namespace.new("facebook_app", :redis => Redis.new)
+
+
+username = ENV["REDIS_USR"]
+password = ENV["REDIS_PW"]
+
+uri = URI.parse(ENV["REDISTOGO_URL"])
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
